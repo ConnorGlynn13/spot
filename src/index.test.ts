e00b1d7d1076d5fcb13d59941a646ba5a4fc25ba
ConnorGlynn13 <connorglynn13@GMAIL.COM>
2020-07-19 21:56:24 +0100

    Add basic homepage with stub data

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,9 +4,9 @@ import supertest from 'supertest'
 const request = supertest(app)
 
 test('home route', async () => {
-    const response = await request.get('/')
+    const response = await request.get('/ ')
     expect(response.status).toBe(200)
-    expect(response.text).toBe('Hello world!')
+    expect(response.text).toContain('<h1>Spot</h1>')
 })
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,40 @@
 import express from 'express'
+import nunjucks from 'nunjucks'
 
 const app = express()
 const PORT = process.env.PORT || '3000'
 
-app.get('/', (request, response) => {
-    response.send("Hello world!")
+nunjucks.configure('./public/views', {
+    autoescape: true,
+    express: app
 })
+
+app.get('/', (request, response) => {
+    return response.render('index.njk', getServices())
+})
+
+const getServices = () => {
+    return {
+        services: [
+            {
+                name: 'prisonstaffhub',
+                home: 'https://sign-in-dev.hmpps.service.justice.gov.uk/auth/login',
+                repository: 'https://github.com/ministryofjustice/prisonstaffhub',
+                deploymentPipeline: '',
+                image: 'https://hub.docker.com/r/mojdigitalstudio/prisonstaffhub',
+                documentation: '',
+            },
+            {
+                name: 'something else',
+                home: '',
+                repository: '',
+                deploymentPipeline: '',
+                image: '',
+                documentation: '',
+            },
+        ]
+    }
+}
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`)
